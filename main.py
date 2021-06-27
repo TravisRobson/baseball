ac5e86e9007434f1795fc467a778f6ba24d11284
Travis <travisjrobson@gmail.com>
2021-06-27 10:12:42 -0700

    fixed bottom of 9th logic for ending game

--- main.py
+++ main.py
@@ -16,26 +16,23 @@ class Score:
     home: int = 0
     away: int = 0
 
-#def half_inning(inning):
-#    if inning - np.floor(inning) == 0.0:
-#        return False
-#    return True
+def bottom_of(inning):
+    if inning - np.floor(inning) == 0.0:
+        return False
+    return True
 
 def game_over(score, inning):
     # This value used for easy debugging (i.e. to create short games)
     max_innings = 9.0 
     #max_innings = 1.0
-#    if inning >= max_innings - 0.5:
-#        if score.home > score.away:
-#            return True
-#    elif inning >= max_innings:
-#        if score.home != score.away:
-#            return True
-#    return False
-    # @todo TJR: Fix logic for home team not needing to play bottom if they're winning.
-    if inning > max_innings:
-        if score.home != score.away:
+
+    if inning >= max_innings:
+        if score.home > score.away:
             return True
+        if bottom_of(inning):
+            if score.home != score.away:
+                return True
+
     return False
 
 def play_ball():
@@ -44,40 +41,36 @@ def play_ball():
     batting_team = 'away'
     inning = 1.0
 
-    matrix = np.load('data/transitionMatrix.npy')
+    matrix = np.load('data/transitionMatrixBatByBat.npy')
     assert(matrix.shape[0] == len(bb.possible_states))
     assert(matrix.shape[0] == matrix.shape[1])
+    #print(matrix[0,:])
 
     # Execute every inning
     while True:
-        print(f'Inning {inning}, batting {batting_team}, score: home ({score.home}), away({score.away})')
+        #print(f'Inning {inning}, batting {batting_team}, score: home ({score.home}), away({score.away})')
         state = (bb.OutStates.ZERO, bb.RunnerStates.NONE)
 
         at_bats = 0
         while bb.num_outs(state) < 3: # half-inning
             # Sample a state transition 
-            # @todo TJR: 1.0e-7 close enough for floating points?
             index = bb.get_index(state)
             trans_probs = matrix[index, :]
-            assert np.isclose(np.sum(trans_probs), 1.0, 1.0e-7)
+            assert np.isclose(np.sum(trans_probs), 1.0, 1.0e-7) # @todo TJR: 1.0e-7 close enough for floating points?
             sample = ss.multinomial.rvs(n=1, p=trans_probs)
-            #print(trans_probs)
             new_index = np.where(sample == 1)[0][0]
             new_state = bb.possible_states[new_index]
-            #print(new_state)
 
             # Calculate the number of runs scored
             runs = bb.runs_scored(state, new_state)
-            #print(f'runs {runs}')
+            #print(f'runs {runs}, outs {bb.num_outs(new_state)}')
             if batting_team == 'away':
                 score.away += runs
             else:
                 score.home += runs
 
-            # @todo TJR: Deep copy is needed right?
-            state = copy.deepcopy(new_state)
-            #print(f'Num outs {bb.num_outs(state)}')
-            #print(f'state {state}')
+            
+            state = copy.deepcopy(new_state) # @todo TJR: Deep copy is needed right?
             
             # Prevent infinite loops from (likely) invalid transition matrices.
             at_bats += 1
@@ -103,7 +96,7 @@ def play_ball():
 
 if __name__ == '__main__':
     """Run many games and calculate some stats."""
-    games = 1
+    games = 1000
     scores = np.zeros([games, 2])
     for i in range(games):
         score = play_ball()
@@ -112,6 +105,7 @@ if __name__ == '__main__':
     team_one_wins = len(np.where(scores[:,0] > scores[:,1])[0])
     print(f'Team one probability: {team_one_wins/games}')
     print(f'Mean scores: {np.mean(scores)}')
+    print(f'std  scores: {np.std(scores)}')
 
 
     
--- tests/test_runner_advance.py
+++ tests/test_runner_advance.py
@@ -7,7 +7,11 @@ import baseball.baseball as bb
 @pytest.mark.parametrize("a, b, expected_runs", [
     (bb.possible_states[-1], bb.possible_states[-1], 3),
     (bb.possible_states[0], bb.possible_states[0], 1),
+    (bb.possible_states[0], bb.possible_states[1], 0),
 ])
 def test_num_runs(a, b, expected_runs):
     assert bb.runs_scored(a, b) == expected_runs
 
+
+def test_num_outs(a, expected_outs):
+    assert bb.num_outs(a) == expected_outs
