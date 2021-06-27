67756d5dbf18f879812ca4e40380234cde14c141
Travis <travisjrobson@gmail.com>
2021-06-26 21:18:29 -0700

    fixed inning logic, ish

--- main.py
+++ main.py
@@ -16,19 +16,27 @@ class Score:
     home: int = 0
     away: int = 0
 
+#def half_inning(inning):
+#    if inning - np.floor(inning) == 0.0:
+#        return False
+#    return True
 
 def game_over(score, inning):
-
-    #max_innings = 9.0 # This value used for easy debugging (i.e. to create short games)
-    max_innings = 1.0
-    if inning >= max_innings - 0.5:
-        if score.home > score.away:
-            return True
-    elif inning >= max_innings:
+    # This value used for easy debugging (i.e. to create short games)
+    max_innings = 9.0 
+    #max_innings = 1.0
+#    if inning >= max_innings - 0.5:
+#        if score.home > score.away:
+#            return True
+#    elif inning >= max_innings:
+#        if score.home != score.away:
+#            return True
+#    return False
+    # @todo TJR: Fix logic for home team not needing to play bottom if they're winning.
+    if inning > max_innings:
         if score.home != score.away:
             return True
     return False
-
 
 def play_ball():
     score = Score()
@@ -39,15 +47,13 @@ def play_ball():
     matrix = np.load('data/transitionMatrix.npy')
     assert(matrix.shape[0] == len(bb.possible_states))
     assert(matrix.shape[0] == matrix.shape[1])
-    print(bb.possible_states)
 
     # Execute every inning
     while True:
-        print(f'Inning {inning}, score: home ({score.home}), away({score.away})')
+        print(f'Inning {inning}, batting {batting_team}, score: home ({score.home}), away({score.away})')
         state = (bb.OutStates.ZERO, bb.RunnerStates.NONE)
-        
 
-        count = 0
+        at_bats = 0
         while bb.num_outs(state) < 3: # half-inning
             # Sample a state transition 
             # @todo TJR: 1.0e-7 close enough for floating points?
@@ -55,14 +61,14 @@ def play_ball():
             trans_probs = matrix[index, :]
             assert np.isclose(np.sum(trans_probs), 1.0, 1.0e-7)
             sample = ss.multinomial.rvs(n=1, p=trans_probs)
-            print(trans_probs)
+            #print(trans_probs)
             new_index = np.where(sample == 1)[0][0]
             new_state = bb.possible_states[new_index]
             #print(new_state)
 
             # Calculate the number of runs scored
             runs = bb.runs_scored(state, new_state)
-            print(f'runs {runs}')
+            #print(f'runs {runs}')
             if batting_team == 'away':
                 score.away += runs
             else:
@@ -70,13 +76,13 @@ def play_ball():
 
             # @todo TJR: Deep copy is needed right?
             state = copy.deepcopy(new_state)
-            print(f'Num outs {bb.num_outs(state)}')
-            print(f'state {state}')
+            #print(f'Num outs {bb.num_outs(state)}')
+            #print(f'state {state}')
             
-            # Prevent infinite loops from invalid transition matrices.
-            count += 1
-            if count > 100:
-                raise('Infinite loop for batters.')
+            # Prevent infinite loops from (likely) invalid transition matrices.
+            at_bats += 1
+            if at_bats > 100:
+                raise Exception('Infinite loop for batters.')
 
         # Check if the game is over
         if game_over(score, inning):
@@ -84,14 +90,13 @@ def play_ball():
         elif inning > 20:
             raise Exception('Infinite loop for innings.')            
 
-        #break
         inning += 0.5
 
         # change team up to bat
-        if batting_team == 'away':
-            batting_team == 'home'
+        if batting_team == "away":
+            batting_team = 'home'
         else:
-            batting_team == 'away'
+            batting_team = 'away'
 
     return score
 
